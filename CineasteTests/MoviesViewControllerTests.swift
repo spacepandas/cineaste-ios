a3b4ce4323fab67b6ed0ea6e72159e91ed4592cd
Felizia Bernutz <felizia.bernutz@gmx.de>
2018-01-01 17:57:02 +0100

    Delete unused code

--- CineasteTests/MoviesViewControllerTests.swift
+++ CineasteTests/MoviesViewControllerTests.swift
@@ -24,30 +24,4 @@ class MoviesViewControllerTests: XCTestCase {
         moviesVC.category = .wantToSee
         XCTAssertNotNil(moviesVC.fetchedResultsManager.delegate)
     }
-
-//    func testNumberOfRowsShouldEqualNumberOfStoredMovies() {
-//        XCTAssertEqual(moviesVC.tableView(tableView, numberOfRowsInSection: 0), 0)
-//
-//        moviesVC.fetchedResultsManager.controller?.fetchedObjects = storedMovies
-//
-//        XCTAssertEqual(moviesVC.tableView(tableView, numberOfRowsInSection: 0), 2)
-//    }
-
-//    func testCellForRowShouldBeOfTypeWantToSeeListCell() {
-//        moviesVC.fetchedResultsManager.controller?.fetchedObjects = storedMovies
-//
-//        for row in 0..<storedMovies.count {
-//            let path = IndexPath(row: row, section: 0)
-//            let cell = moviesVC.tableView(tableView, cellForRowAt: path)
-//
-//            XCTAssert(cell is WantToSeeListCell)
-//        }
-//    }
-
-    private let storedMovies: [StoredMovie] = {
-        let managedObjectContext = setUpInMemoryManagedObjectContext()
-        let entity = NSEntityDescription.insertNewObject(forEntityName: "StoredMovie", into: managedObjectContext) as! StoredMovie
-        return [entity, entity]
-    }()
-    
 }
