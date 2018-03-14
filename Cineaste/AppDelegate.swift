c31f476bd22fb17a392caec58f643ccab391daef
Felizia Bernutz <felizia.bernutz@gmx.de>
2018-03-14 18:22:48 +0100

    Update swiftlint
    Update swiftlint because of violations with
    "let_var_whitespace" in Xcode 9.3 beta

--- Cineaste/AppDelegate.swift
+++ Cineaste/AppDelegate.swift
@@ -44,7 +44,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
          error conditions that could cause the creation of the store to fail.
          */
         let container = NSPersistentContainer(name: "Model")
-        container.loadPersistentStores(completionHandler: { (_, error) in
+        container.loadPersistentStores(completionHandler: { _, error in
             if let error = error as NSError? {
                 // Replace this implementation with code to handle the error appropriately.
                 // fatalError() causes the application to generate a crash log and terminate. You should not use this function in a shipping application, although it may be useful during development.
