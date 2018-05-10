4d9d020721b64b0e3e5552839a2b5423fda7824d
Felizia Bernutz <felizia.bernutz@gmx.de>
2018-05-10 10:47:52 +0200

    Add default style for DescriptionTextView in movieDetails

--- Cineaste/Custom UI/TextView.swift
+++ Cineaste/Custom UI/TextView.swift
@@ -9,27 +9,49 @@
 import UIKit
 
 public class DescriptionTextView: UITextView {
-
-    func setup(with type: TextViewType) {
-        isEditable = false
-        dataDetectorTypes = .link
-        linkTextAttributes = [NSAttributedStringKey.foregroundColor.rawValue: UIColor.primaryOrange]
-
-        //Define attributes
+    private let paragraphStyle: NSMutableParagraphStyle = {
         let style = NSMutableParagraphStyle()
         style.lineSpacing = 7
         style.hyphenationFactor = 0.7
+        return style
+    }()
 
-        let titleAttributes = [NSAttributedStringKey.paragraphStyle: style,
+    required public init?(coder aDecoder: NSCoder) {
+        super.init(coder: aDecoder)
+        setup()
+    }
+
+    // swiftlint:disable:next implicitly_unwrapped_optional
+    override public var text: String! {
+        didSet {
+            setup()
+
+            let defaultAttributes = [NSAttributedStringKey.paragraphStyle: paragraphStyle,
+                                     NSAttributedStringKey.font: UIFont.systemFont(ofSize: 16),
+                                     NSAttributedStringKey.foregroundColor: UIColor.basicBackground]
+
+            self.attributedText = NSAttributedString(string: self.text,
+                                                     attributes: defaultAttributes)
+        }
+    }
+
+    func setup() {
+        isEditable = false
+        dataDetectorTypes = .link
+        linkTextAttributes = [NSAttributedStringKey.foregroundColor.rawValue: UIColor.primaryOrange]
+    }
+
+    func setup(with type: TextViewType) {
+        let titleAttributes = [NSAttributedStringKey.paragraphStyle: paragraphStyle,
                                NSAttributedStringKey.font: UIFont.boldSystemFont(ofSize: 18),
                                NSAttributedStringKey.foregroundColor: UIColor.basicBackground]
 
-        let paragraphAttributes = [NSAttributedStringKey.paragraphStyle: style,
+        let paragraphAttributes = [NSAttributedStringKey.paragraphStyle: paragraphStyle,
                                    NSAttributedStringKey.font: UIFont.systemFont(ofSize: 16),
                                    NSAttributedStringKey.foregroundColor: UIColor.accentText]
 
         let chain = type.chainContent(titleAttributes: titleAttributes,
-                               paragraphAttributes: paragraphAttributes)
+                                      paragraphAttributes: paragraphAttributes)
 
         self.attributedText = chain
     }
