924ff77cdcb931011a64a284f3acf0c97a7840fc
Felizia Bernutz <felizia.bernutz@gmx.de>
2018-07-03 22:50:20 +0200

    Add link to tmdb in MovieDetailVC

---
+++ Cineaste/Extension/SFSafariViewController+Style.swift
@@ -0,0 +1,17 @@
+//
+//  SFSafariViewController+Style.swift
+//  Cineaste
+//
+//  Created by Felizia Bernutz on 03.07.18.
+//  Copyright © 2018 notimeforthat.org. All rights reserved.
+//
+
+import UIKit
+import SafariServices
+
+extension SFSafariViewController {
+    func addStyle() {
+        preferredBarTintColor = .safariBarTint
+        preferredControlTintColor = .basicWhite
+    }
+}
