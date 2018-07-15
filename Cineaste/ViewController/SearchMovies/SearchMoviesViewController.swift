ce8c52382aa7947b230dad58b23a5368db512165
Felizia Bernutz <felizia.bernutz@gmx.de>
2018-07-15 14:36:19 +0200

    Reset pagination when search was started or cancelled

--- Cineaste/ViewController/SearchMovies/SearchMoviesViewController.swift
+++ Cineaste/ViewController/SearchMovies/SearchMoviesViewController.swift
@@ -117,6 +117,10 @@ class SearchMoviesViewController: UIViewController {
 
 extension SearchMoviesViewController: UISearchResultsUpdating {
     internal func updateSearchResults(for searchController: UISearchController) {
+        //reset pagination
+        currentPage = nil
+        totalResults = nil
+
         searchDelayTimer?.invalidate()
         searchDelayTimer = Timer.scheduledTimer(withTimeInterval: 0.2, repeats: false) { [weak self] _ in
             self?.loadMovies(forQuery: searchController.searchBar.text) { movies in
