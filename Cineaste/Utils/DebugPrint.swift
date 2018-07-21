cfa122a8120c1c72783fa841d76607a2cbf87b38
Felizia Bernutz <felizia.bernutz@gmx.de>
2018-07-21 11:43:27 +0200

    Only print in DEBUG mode
    Use `releasePrint` to even print in release mode

---
+++ Cineaste/Utils/DebugPrint.swift
@@ -0,0 +1,19 @@
+//
+//  DebugPrint.swift
+//  Cineaste
+//
+//  Created by Felizia Bernutz on 21.07.18.
+//  Copyright © 2018 spacepandas.de. All rights reserved.
+//
+
+import Foundation
+
+func releasePrint(_ object: Any) {
+    Swift.print(object)
+}
+
+func print(_ object: Any) {
+    #if DEBUG
+    Swift.print(object)
+    #endif
+}
