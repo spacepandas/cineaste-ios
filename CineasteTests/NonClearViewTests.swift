e2df02bd19e8e531b274cd77593de5f2f9881739
xaverlohmueller <xaver.lohmueller@me.com>
2018-09-01 12:27:08 +0200

    Fix a visual glitch when selecting cells (#51)

---
+++ CineasteTests/NonClearViewTests.swift
@@ -0,0 +1,38 @@
+//
+//  NonClearViewTests.swift
+//  CineasteTests
+//
+//  Created by Xaver Lohmüller on 19.08.18.
+//  Copyright © 2018 spacepandas.de. All rights reserved.
+//
+
+import XCTest
+@testable import Cineaste_App_Dev
+
+class NonClearViewTests: XCTestCase {
+
+    func testNonClearViewShouldNotAcceptClearColor() {
+        // Given
+        let view = NonClearView()
+        let initialColor = UIColor.red
+        view.backgroundColor = initialColor
+
+        // When
+        view.backgroundColor = .clear
+
+        // Then
+        XCTAssertEqual(view.backgroundColor, initialColor)
+    }
+
+    func testNonClearViewShouldAcceptColorsAboveZeroAlpha() {
+        // Given
+        let view = NonClearView()
+        let notTotallyTransparentColor = UIColor(red: 1, green: 1, blue: 1, alpha: 0.5)
+
+        // When
+        view.backgroundColor = notTotallyTransparentColor
+
+        // Then
+        XCTAssertEqual(view.backgroundColor, notTotallyTransparentColor)
+    }
+}
