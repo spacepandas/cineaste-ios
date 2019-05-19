cbbcdc319402ac671781035f8829cf2b569e9a31
Xaver Lohmüller <xlo@adorsys.de>
2019-05-19 18:11:04 +0200

    Add tests

--- CineasteTests/PersistenceTests.swift
+++ CineasteTests/PersistenceTests.swift
@@ -49,10 +49,29 @@ class PersistenceTests: XCTestCase {
         XCTAssertFalse(movies.isEmpty)
     }
 
+    func testUrlForMovieExportShouldGenerateJSONFileInCachesDirectory() throws {
+        // Given
+        let fileManager = FileManager.default
+        try Persistence.saveMovies([Movie(id: 1)])
+
+        // When
+        let url = try Persistence.urlForMovieExport()
+
+        // Then
+        XCTAssert(fileManager.fileExists(atPath: url.path))
+        let data = try Data(contentsOf: url)
+        let exportedMovies = try JSONDecoder.tmdbDecoder.decode(ImportExportObject.self, from: data).movies
+        XCTAssertEqual(exportedMovies, Persistence.loadMovies())
+    }
+
     private func cleanup() throws {
         let fileManager = FileManager.default
         for content in try fileManager.contentsOfDirectory(atPath: fileManager.documentsDirectory.path) {
             let url = fileManager.documentsDirectory.appendingPathComponent(content)
+            try fileManager.removeItem(at: url)
+        }
+        for content in try fileManager.contentsOfDirectory(atPath: fileManager.cachesDirectory.path) {
+            let url = fileManager.cachesDirectory.appendingPathComponent(content)
             try fileManager.removeItem(at: url)
         }
     }
@@ -62,6 +81,14 @@ private extension FileManager {
     var documentsDirectory: URL {
         // swiftlint:disable:next force_try
         return try! url(for: .documentDirectory,
+                        in: .userDomainMask,
+                        appropriateFor: nil,
+                        create: false)
+    }
+
+    var cachesDirectory: URL {
+        // swiftlint:disable:next force_try
+        return try! url(for: .cachesDirectory,
                         in: .userDomainMask,
                         appropriateFor: nil,
                         create: false)
