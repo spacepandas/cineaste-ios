94151e72b03f4383ceea3aacae681ad0a9e7c47d
Felizia Bernutz <felizia.bernutz@gmx.de>
2019-05-19 16:34:18 +0200

    Format code

--- Cineaste/AppDelegate.swift
+++ Cineaste/AppDelegate.swift
@@ -24,7 +24,7 @@ class AppDelegate: UIResponder, UIApplicationDelegate {
     var paws: MonkeyPaws?
     #endif
 
-    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey : Any]? = nil) -> Bool {
+    func application(_ application: UIApplication, willFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]? = nil) -> Bool {
 
         // IMPORTANT: Call this before the ReSwift Store is initialized
         // (before ReSwiftInit is dispatched)
