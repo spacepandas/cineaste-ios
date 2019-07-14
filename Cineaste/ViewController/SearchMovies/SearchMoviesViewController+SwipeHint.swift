2d4c418727e30046ff77a13c4f06e20ea4e260d4
wtimme <wtimme@users.noreply.github.com>
2019-07-14 20:51:54 +0200

    #98 Only show "Swipe action" hint once
    The number of times that the hint should be shown
    can be configured using the constant.

--- Cineaste/ViewController/SearchMovies/SearchMoviesViewController+SwipeHint.swift
+++ Cineaste/ViewController/SearchMovies/SearchMoviesViewController+SwipeHint.swift
@@ -13,6 +13,30 @@ extension SearchMoviesViewController {
     func animateSwipeActionHint() {
         guard let cell = tableView.visibleCells.first as? SearchMoviesCell else { return }
 
+        guard shouldAnimateShowSwipeActionHint else { return }
+
         cell.animateSwipeHint()
+
+        rememberThatSwipeActionHintWasAnimated()
+    }
+
+    /// MARK: Persistency
+
+    private var swipeActionHintShowCountKey: String {
+        return "cineaste-swipeActionHintShowCount"
+    }
+
+    /// Determines whether the swipe action hint should be animated.
+    private var shouldAnimateShowSwipeActionHint: Bool {
+        let maximumNumberOfTimesToShowHint = 1
+        let numberOfTimesHintWasShown = UserDefaults.standard.integer(forKey: swipeActionHintShowCountKey)
+
+        return numberOfTimesHintWasShown < maximumNumberOfTimesToShowHint
+    }
+
+    private func rememberThatSwipeActionHintWasAnimated() {
+        let numberOfTimesHintWasShown = UserDefaults.standard.integer(forKey: swipeActionHintShowCountKey)
+
+        UserDefaults.standard.set(numberOfTimesHintWasShown + 1, forKey: swipeActionHintShowCountKey)
     }
 }
