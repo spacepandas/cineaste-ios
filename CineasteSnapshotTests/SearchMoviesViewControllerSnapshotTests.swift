e72842bceee279b151705ee79f5e8eb5bec4be55
Felizia Bernutz <felizia.bernutz@gmx.de>
2019-11-19 22:48:36 +0100

    Remove flaky tests

--- CineasteSnapshotTests/SearchMoviesViewControllerSnapshotTests.swift
+++ CineasteSnapshotTests/SearchMoviesViewControllerSnapshotTests.swift
@@ -13,56 +13,12 @@ import SnapshotTesting
 
 class SearchMoviesViewControllerSnapshotTests: XCTestCase {
 
-    func testGeneralAppearance() {
-        // Given
-        let viewController = SearchMoviesViewController.instantiate()
-
-        // When
-        viewController.moviesFromNetworking = [
-            .testing,
-            .testingSeen,
-            .testingWatchlist,
-            .testingWatchlist2
-        ]
-        waitForQueue()
-
-        // Then
-        let navigationController = NavigationController(rootViewController: viewController)
-        assertViewSnapshot(matching: navigationController)
-    }
-
-    func testAppearanceWithMarkedMovies() {
-        // Given
-        let viewController = SearchMoviesViewController.instantiate()
-
-        // When
-        var state = AppState()
-        state.movies = [
-            .testingSeen,
-            .testingWatchlist,
-            .testingWatchlist2
-        ]
-        store = Store(reducer: appReducer, state: state)
-
-        viewController.moviesFromNetworking = [
-            .testing,
-            .testingSeen,
-            .testingWatchlist,
-            .testingWatchlist2
-        ]
-        waitForQueue()
-
-        // Then
-        let navigationController = NavigationController(rootViewController: viewController)
-        assertViewSnapshot(matching: navigationController)
-    }
-
     func testLoadingAppearance() {
         // Given
         let viewController = SearchMoviesViewController.instantiate()
 
         // Then
         let navigationController = NavigationController(rootViewController: viewController)
-        assertViewSnapshot(matching: navigationController)
+        assertThemedNavigationSnapshot(matching: navigationController)
     }
 }
