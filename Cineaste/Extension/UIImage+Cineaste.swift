0467ba2edc844f0786c98f57ca652b4424becd90
Xavier Lowmiller <xaver.lohmueller@me.com>
2020-01-27 13:32:48 +0100

    Use SF Symbols on iOS 13 and later

--- Cineaste/Extension/UIImage+Cineaste.swift
+++ Cineaste/Extension/UIImage+Cineaste.swift
@@ -10,10 +10,25 @@ import UIKit
 
 extension UIImage {
     static let posterPlaceholder = #imageLiteral(resourceName: "placeholder_poster")
-    static let watchlistIcon = #imageLiteral(resourceName: "watchlist")
     static let seenBadgeIcon = #imageLiteral(resourceName: "seen-badge")
     static let watchlistBadgeIcon = #imageLiteral(resourceName: "watchlist-badge")
-    static let seenIcon = #imageLiteral(resourceName: "seen")
-    static let moreIcon = #imageLiteral(resourceName: "more")
-    static let searchIcon = #imageLiteral(resourceName: "search")
+    static var watchlistIcon = image(systemName: "star.fill", fallback: #imageLiteral(resourceName: "watchlist"))
+    static var seenIcon = image(systemName: "checkmark", fallback: #imageLiteral(resourceName: "seen"))
+    static var moreIcon = image(systemName: "ellipsis", fallback: #imageLiteral(resourceName: "more"))
+    static var searchIcon = image(systemName: "magnifyingglass", fallback: #imageLiteral(resourceName: "search"))
+}
+
+/// Creates an image object containing a system symbol image, if available
+/// - Parameters:
+///   - systemName: The name of the system symbol image.
+///   Only respected in iOS 13 and later.
+///   - fallback: Fallback image for iOS 12 and earlier
+private func image(systemName: String, fallback: UIImage) -> UIImage {
+    if #available(iOS 13, *) {
+        // It's better to crash here than have undefined UI
+        // swiftlint:disable:next force_unwrapping
+        return UIImage(systemName: systemName)!
+    } else {
+        return fallback
+    }
 }
