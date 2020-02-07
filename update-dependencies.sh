77767712d5781701593cf2a7f88c02f867934484
Xavier Lowmiller <xaver.lohmueller@me.com>
2020-02-07 14:27:31 +0100

    Use update-dependencies.sh for license-plist generation

--- update-dependencies.sh
+++ update-dependencies.sh
@@ -1,4 +1,19 @@
 #!/usr/bin/env bash
 
+license-plist() {
+    if which license-plist >/dev/null; then
+        license-plist \
+            --output-path Cineaste/Settings.bundle \
+            --package-path Cineaste.xcodeproj/project.xcworkspace/xcshareddata/swiftpm/Package.swift \
+            --prefix Acknowledgements \
+            --single-page \
+            --force
+    else
+        echo "warning: LicensePlist not installed, download from https://github.com/mono0926/LicensePlist"
+        exit -1
+    fi
+}
+
 bundle update
-bundle exec fastlane snapshot update
+bundle exec fastlane snapshot update --force
+license-plist
