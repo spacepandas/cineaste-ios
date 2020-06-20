16c81688d4ce0c805f2246e2f6c0972adf9036cf
Xavier Lowmiller <xaver.lohmueller@me.com>
2020-06-20 17:38:40 +0200

    Show UISearchToken in search field

--- Cineaste/ViewController/SearchMovies/SearchMoviesViewController+UITableView.swift
+++ Cineaste/ViewController/SearchMovies/SearchMoviesViewController+UITableView.swift
@@ -30,8 +30,25 @@ extension SearchMoviesViewController: UITableViewDataSourcePrefetching {
 
 extension SearchMoviesViewController: UITableViewDelegate {
     func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
-        let selectedMovie = movies[indexPath.row]
-        store.dispatch(SelectionAction.select(movie: selectedMovie))
-        perform(segue: .showMovieDetail, sender: self)
+
+        switch (dataSource.mode, SearchSection(rawValue: indexPath.section)) {
+        case (.discover, _),
+             (.manualSearch, .movies?):
+            let selectedMovie = movies[indexPath.row]
+            store.dispatch(SelectionAction.select(movie: selectedMovie))
+            perform(segue: .showMovieDetail, sender: self)
+        case (.manualSearch, .tokens?):
+            guard #available(iOS 13.0, *),
+                let cell = tableView.cellForRow(at: indexPath) as? SearchTokenCell,
+                let genre = cell.title.text
+                else { return }
+
+            let searchField = resultSearchController.searchBar.searchTextField
+            let searchToken = UISearchToken(icon: UIImage.moreIcon, text: genre)
+            searchField.insertToken(searchToken, at: 0)
+            searchField.tokenBackgroundColor = .cineButton
+        default:
+            break
+        }
     }
 }
