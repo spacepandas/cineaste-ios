40d73123803c7a0e4b94a5140d9f0c886167c1e3
Felizia Bernutz <felizia.bernutz@gmx.de>
2020-10-04 17:29:55 +0200

    Use Extension in CountdownView as well

--- MovieReleaseWidgetExtension/CountdownWidget/View/CountdownView.swift
+++ MovieReleaseWidgetExtension/CountdownWidget/View/CountdownView.swift
@@ -29,21 +29,9 @@ struct CountdownView: SwiftUI.View {
     }
     @State private var textHeight: CGFloat = 45
 
-    var difference: String {
-        let formatter = DateComponentsFormatter()
-        formatter.allowedUnits = [.day, .weekOfMonth]
-        formatter.unitsStyle = .full
-        formatter.maximumUnitCount = 1
-        formatter.collapsesLargestUnit = true
-        let releaseDate = movie.releaseDate ?? Date()
-        let formattedReleaseDate = formatter.string(from: Date(), to: releaseDate + 24 * 60 * 60) ?? ""
-
-        return formattedReleaseDate
-    }
-
     var accessibilityHint: String {
         let format = NSLocalizedString("movie_release_widget_upcoming_accessibility_format", comment: "")
-        return String(format: format, movie.title, difference)
+        return String(format: format, movie.title, movie.difference)
     }
 
     var body: some SwiftUI.View {
@@ -62,7 +50,7 @@ struct CountdownView: SwiftUI.View {
                     .frame(height: textHeight)
 
                 HStack(alignment: .bottom, spacing: 0) {
-                    Text(difference.split(separator: " ")[0])
+                    Text(movie.difference.split(separator: " ")[0])
                         .font(Font.custom("Noteworthy", fixedSize: 500))
                         .bold()
                         .minimumScaleFactor(0.01)
@@ -71,7 +59,7 @@ struct CountdownView: SwiftUI.View {
                         .outlined(radius: outlineRadius)
                         .accessibility(hidden: true)
                     VStack(alignment: .leading) {
-                        Text(difference.split(separator: " ")[1].uppercased())
+                        Text(movie.difference.split(separator: " ")[1].uppercased())
                             .font(Font.custom("Noteworthy", fixedSize: 24))
                             .bold()
                             .minimumScaleFactor(0.01)
