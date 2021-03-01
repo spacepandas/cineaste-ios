6b7b71d782c79ef349ad0e3c2b8a4bffcf9025d5
Felizia Bernutz <felizia.bernutz@gmx.de>
2021-03-01 14:52:14 +0100

    Add Collage of Screenshots in Readme (#168)
    * Add Collection of Screenshots to README.md
    
    * Add more configuration for UITest
    
    * Make more screenshots from different devices
    
    * Move image
    
    * Add alt tags for images

--- CineasteUITests/ScreenshotsUITests.swift
+++ CineasteUITests/ScreenshotsUITests.swift
@@ -30,59 +30,58 @@ class ScreenshotsUITests: XCTestCase {
         ]
 
         setupSnapshot(app, waitForAnimations: false)
-        app.launch()
-
-        resetMoviesIfNeeded()
     }
 
     func testScreenshots() {
+        app.launch()
+
+        resetMoviesIfNeeded()
+
         let backButton = app.navigationBars.buttons.element(boundBy: 0).firstMatch
 
-        // Empty Watchlist
-        XCTAssertEqual(app.cells.count, 0)
-
-        XCTContext.runActivity(named: "Search for Movies without Markers") { _ in
-            app.tabBars.buttons["SearchTab"].firstMatch.tap()
-
-            // Check that search is not empty
-            let firstCellInSearch = app.tables["Search.TableView"].cells.element(boundBy: 0).firstMatch
-            let exists = NSPredicate(format: "exists == true")
-            expectation(for: exists, evaluatedWith: firstCellInSearch, handler: nil)
-            waitForExpectations(timeout: 2, handler: nil)
-            XCTAssert(firstCellInSearch.exists)
-        }
-
-        XCTContext.runActivity(named: "Add first Movie to Watchlist from Search Movie Detail") { _ in
-            markMovieFromSearch(index: 0, asWatched: false)
-        }
-
-        XCTContext.runActivity(named: "Mark second Movie as watched from Search Movie Detail") { _ in
-            markMovieFromSearch(index: 1, asWatched: true)
-        }
-
-        XCTContext.runActivity(named: "Mark fourth Movie as watched from Search Movie Detail") { _ in
-            markMovieFromSearch(index: 3, asWatched: true)
-        }
-
-        XCTContext.runActivity(named: "See Search with marked Movies") { _ in
-            namedSnapshot("02_search")
-        }
+        screenshotSearchWithMarkers()
 
         XCTContext.runActivity(named: "See Watchlist") { _ in
             app.tabBars.buttons["WatchlistTab"].firstMatch.tap()
-            XCTAssertEqual(app.cells.count, 1)
+            XCTAssertNotEqual(app.cells.count, 0)
             namedSnapshot("03_watchlist")
+        }
 
-            // Watchlist Detail
+        XCTContext.runActivity(named: "See Watchlist iPhone Landscape") { _ in
+            if UIDevice.current.userInterfaceIdiom == .phone {
+                XCUIDevice.shared.orientation = .landscapeLeft
+
+                namedSnapshot("03_watchlist_landscape")
+
+                XCUIDevice.shared.orientation = .portrait
+            }
+        }
+
+        XCTContext.runActivity(named: "See Watchlist Detail") { _ in
             let wantToSeeMovie = app.cells.element(boundBy: 0).firstMatch
             wantToSeeMovie.tap()
+
             namedSnapshot("01_watchlist_detail")
+        }
+
+        XCTContext.runActivity(named: "See Watchlist Detail iPad Landscape") { _ in
+            if UIDevice.current.userInterfaceIdiom == .pad {
+                XCUIDevice.shared.orientation = .landscapeLeft
+
+                app.scrollDownToElement(element: app.segmentedControls.firstMatch)
+                app.scrollDown()
+
+                namedSnapshot("01_watchlist_detail_landscape")
+
+                XCUIDevice.shared.orientation = .portrait
+            }
+
             backButton.tap()
         }
 
         XCTContext.runActivity(named: "See Seen List") { _ in
             app.tabBars.buttons["SeenTab"].firstMatch.tap()
-            XCTAssertEqual(app.cells.count, 2)
+            XCTAssertNotEqual(app.cells.count, 0)
             namedSnapshot("04_seenList")
 
             // History Detail
@@ -103,6 +102,15 @@ class ScreenshotsUITests: XCTestCase {
             let license = app.cells.element(boundBy: 1).firstMatch
             license.tap()
         }
+    }
+
+    func testScreenshotsOfSearchWithDarkMode() {
+        app.launchArguments += [
+            "UI_TEST_DARK_MODE"
+        ]
+        app.launch()
+
+        screenshotSearchWithMarkers()
     }
 }
 
@@ -137,11 +145,50 @@ extension ScreenshotsUITests {
         XCTAssertEqual(app.cells.count, 0)
     }
 
-    private func namedSnapshot(_ name: String) {
-        if app.launchArguments.contains("UICTContentSizeCategoryAccessibilityL") {
-            snapshot(name + "_a11y", timeWaitingForIdle: 0)
-        } else {
-            snapshot(name, timeWaitingForIdle: 0)
+    private func screenshotSearchWithMarkers() {
+        XCTContext.runActivity(named: "Search for Movies without Markers") { _ in
+            app.tabBars.buttons["SearchTab"].firstMatch.tap()
+
+            // Check that search is not empty
+            let firstCellInSearch = app.tables["Search.TableView"].cells.element(boundBy: 0).firstMatch
+            let exists = NSPredicate(format: "exists == true")
+            expectation(for: exists, evaluatedWith: firstCellInSearch, handler: nil)
+            waitForExpectations(timeout: 2, handler: nil)
+            XCTAssert(firstCellInSearch.exists)
         }
+
+        XCTContext.runActivity(named: "Add sixth Movie to Watchlist from Search Movie Detail") { _ in
+            markMovieFromSearch(index: 5, asWatched: false)
+        }
+
+        XCTContext.runActivity(named: "Mark second Movie as watched from Search Movie Detail") { _ in
+            markMovieFromSearch(index: 1, asWatched: true)
+        }
+
+        XCTContext.runActivity(named: "Mark fourth Movie as watched from Search Movie Detail") { _ in
+            markMovieFromSearch(index: 3, asWatched: true)
+        }
+
+        XCTContext.runActivity(named: "Add first Movie to Watchlist from Search Movie Detail") { _ in
+            markMovieFromSearch(index: 0, asWatched: false)
+        }
+
+        XCTContext.runActivity(named: "See Search with marked Movies") { _ in
+            namedSnapshot("02_search")
+        }
+    }
+
+    private func namedSnapshot(_ name: String) {
+        var snapshotName = name
+
+        if app.launchArguments.contains("UICTContentSizeCategoryAccessibilityL") {
+            snapshotName += "_a11y"
+        }
+
+        if app.launchArguments.contains("UI_TEST_DARK_MODE") {
+            snapshotName += "_dark"
+        }
+
+        snapshot(snapshotName, timeWaitingForIdle: 0)
     }
 }
