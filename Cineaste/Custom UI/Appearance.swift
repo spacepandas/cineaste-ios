5e9a9e0787ef658e0fbd14dd8b26e57512ff39ac
Felizia Bernutz <felizia.bernutz@gmx.de>
2021-07-24 13:25:35 +0200

    Use UITabBarItemAppearance and UITabBarAppearance to configure tab bar

--- Cineaste/Custom UI/Appearance.swift
+++ Cineaste/Custom UI/Appearance.swift
@@ -10,28 +10,6 @@ import UIKit
 
 enum Appearance {
     static func setup() {
-        // MARK: TabBar
-        let tabBar = UITabBar.appearance()
-        tabBar.isTranslucent = false
-        tabBar.tintColor = .cineTabBarSelected
-        tabBar.barTintColor = .cineTabBar
-        tabBar.unselectedItemTintColor = .cineTabBarNormal
-
-        // MARK: TabBarItem
-        let tabBarItem = UITabBarItem.appearance()
-        tabBarItem.setTitleTextAttributes(
-            [
-                .foregroundColor: UIColor.cineTabBarSelected
-            ],
-            for: .selected
-        )
-        tabBarItem.setTitleTextAttributes(
-            [
-                .foregroundColor: UIColor.cineTabBarNormal
-            ],
-            for: .normal
-        )
-
         // MARK: UIAlertController
         let alertController = UIView
             .appearance(whenContainedInInstancesOf: [UIAlertController.self])
